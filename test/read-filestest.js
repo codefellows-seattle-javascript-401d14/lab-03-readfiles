da5a18f362d621eabbb2f8689f4428fa55799ad6
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-01-06 18:54:24 -0800

    lab 3 compelted

--- test/read-filestest.js
+++ test/read-filestest.js
@@ -1,39 +1,52 @@
 'use strict';
-
 const fs = require('fs');
 const expect = require('chai').expect;
 const myreadfile = require('../lib/read-files.js');
 
-let bacon = `${__dirname}/assets/bacon.txt`;
-let hipster = `${__dirname}/assets/hipster.txt`;
-let mobydick = `${__dirname}/assets/mobydick.txt`;
+let bacon = `${__dirname}/../assets/bacon.txt`;
+let hipster = `${__dirname}/../assets/hipster.txt`;
+let mobydick = `${__dirname}/../assets/mobydick.txt`;
 
+let filePath = [bacon, hipster, mobydick];
 
-// describe('testing read-file module', function(){
-//     describe('with valid inputs', function (){
-//         before(function(done){
-            
-//         })   
-//     })
+describe('testing the file read module', function(){
+  let testData = [];
+  describe('testing valid input which is the correct file name', function(){
+    before(function(done){
+      fs.readFile(filePath[0] , 'utf8', function(err, data){
+        if (err) return done(err);
+        testData.push(data);
 
-//       // for async testing your it function must have a done callback
-//     it('passes an error into the callback if any of the three file paths are a file that do not exist', function(done){
-//       let message = 'A path you entered does not exist';
+        fs.readFile(filePath[1] , 'utf8', function(err, data){
+          if (err) return done(err);
+          testData.push(data);
 
-//       // call our function so that we can test it
-//       readfiles(textarrayofIpsum, message, function(err, data){
+          fs.readFile(filePath[2] , 'utf8', function(err, data){
+            if (err) return done(err);
+            testData.push(data);
+            done();
+          });
+        });
+      });
+    }); //end of before block
+    it('should return exact match of text', function(done){  //it blocks should always have a done
+      myreadfile(filePath, function(err, data){
+        if(err) return done(err);
+        expect(testData[0]).to.equal(data[0]);   //exact arrays will never match because of memory isses, so must compares the indexes of arrays
+        expect(testData[1]).to.equal(data[1]);
+        expect(testData[2]).to.equal(data[2]);
+        done(); //done should always do inside the block
+      }); // end of myreadfile block
+    }); // end of 'exact match of text'it block
+  });// end of second describe block
 
-//         // if there was an error call done(err) to tell mocha the test failed
-//         // dont execute any futher if the its allready failed
-//         if(err) return done(err);
-
-//         // run our assertions
-//         expect(data).to.equal(originalText + message);
-        
-//         // tell mocha the test has finished, so it can run the next test
-//         // if you dont do this mocha will assume the test failed and  you
-//         // will get a timeout error
-//         done();
-//       });
-//     });
-
+  describe('testing for invalid data', function(){
+    it('should return not valid data error', function(done){ //it blocks should always have a done
+      let badFilePath = ['/../assets/badfg.txt', '/../adfgdfgs/bacon.tx', '/../assets/bacon.jpg'];
+      myreadfile(badFilePath, function(err){
+        expect(!!err).to.equal(true);  // "expect there to be an error"
+        done();
+      }); //end of badFilePath block
+    });//end of not valid data error block
+  }); //end of invalid data block
+}); // end of describe block
